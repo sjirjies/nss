59e9c9e50b5172300d6e585485b7bd417a0ad278
Saman Jirjies <saman.jirjies@gmail.com>
2014-12-18 16:28:12 -0700

    Cleaned up the code a bit.
    Created SimulationData class for tracking simulation metrics. It is also now responsible for the graph creation.
    Simplified simulation options into a run_simulation function.
    Moved create_basic_intelligence out of GraphicalSimulation as a function since it can be used without graphics.

--- simulation.py
+++ simulation.py
@@ -19,15 +19,15 @@ class World:
         # Update all plants then all bots
         for array in [self.plants, self.bots, self.signals]:
             for entity in list(array):
-                entity.step()
                 # Make sure the entity wraps around the boundaries
                 if self.boundary_sizes:
                     entity.x = entity.x % self.boundary_sizes[0]
                     entity.y = entity.y % self.boundary_sizes[1]
+                entity.step()
                 # Take care of dead entities
                 if entity.dead:
-                    if isinstance(entity, Bot):
-                        print("%s has died" % str(entity))
+                    # if isinstance(entity, Bot):
+                    #    print("%s has died" % str(entity))
                     array.remove(entity)
 
     def add_bot(self, bot):
@@ -44,57 +44,120 @@ class World:
         self.plants.append(plant)
 
 
-def no_graphics_run():
+def create_basic_intelligence():
+    check_reproduce_node = ConditionalNode(Bot.can_i_reproduce)
+    create_clone_node = StatementNode(Bot.create_clone)
+    launch_signal_node = StatementNode(Bot.launch_signal)
+    check_active_signal_node = ConditionalNode(Bot.do_i_have_a_signal)
+    check_signal_found_food_node = ConditionalNode(Bot.has_signal_found_food)
+    wait_node = StatementNode(Bot.wait)
+    move_to_target_node = StatementNode(Bot.move_towards_target)
+    check_at_target_node = ConditionalNode(Bot.am_i_near_target)
+    eat_node = StatementNode(Bot.eat_nearby_food)
 
-    def run(ticks):
+    check_reproduce_node.assign_edges(create_clone_node, launch_signal_node)
+    create_clone_node.assign_edge(check_reproduce_node)
+    launch_signal_node.assign_edge(check_signal_found_food_node)
+    check_signal_found_food_node.assign_edges(move_to_target_node, wait_node)
+    wait_node.assign_edge(check_active_signal_node)
+    check_active_signal_node.assign_edges(check_signal_found_food_node, launch_signal_node)
+    move_to_target_node.assign_edge(check_at_target_node)
+    check_at_target_node.assign_edges(eat_node, move_to_target_node)
+    eat_node.assign_edge(check_reproduce_node)
+
+    behavior = BehaviorTree()
+    behavior.behavior_nodes = [launch_signal_node, check_reproduce_node, create_clone_node,
+                               check_signal_found_food_node, wait_node,move_to_target_node, check_at_target_node,
+                               eat_node, check_active_signal_node]
+    behavior.current_behavior_node = launch_signal_node
+    return behavior
+
+
+def no_graphics_run(world, plant_growth_ticks, additional_ticks, collect_data=True):
+    if collect_data:
+        sim_data = SimulationData(world)
+    else:
+        sim_data = None
+    plant_numbers = []
+    bot_numbers = []
+
+    def run(ticks, with_bots):
         for tick in range(ticks):
-            earth.step()
+            world.step()
             # Keep track of some info for graphing
-            plant_numbers.append(len(earth.plants))
-            bot_numbers.append(len(earth.bots))
+            if sim_data:
+                sim_data.poll_world_for_data()
+            if with_bots and len(world.bots) == 0:
+                print("Stopping %s ticks in since no more bots." % tick)
+                return
+            if tick % 100 == 0:
+                print(" Finished Tick %d / %d" % (tick, ticks))
 
-    run(500)
+    center = world.boundary_sizes[0]/2, world.boundary_sizes[1]/2
+    world.add_plant(Plant(center[0], center[1], 50))
+    print("Running %s ticks with just plants..." % plant_growth_ticks)
+    run(plant_growth_ticks, with_bots=False)
+    behavior = create_basic_intelligence()
+    world.add_bot(Bot(center[0], center[1], 300, behavior))
+    print("Running %s ticks with bots added..." % additional_ticks)
+    run(additional_ticks, with_bots=True)
+    if sim_data:
+        sim_data.graph_results()
 
-    run(1000)
 
-    def graph_results():
+class SimulationData:
+    def __init__(self, world):
+        self.world = world
+        self.plant_numbers = []
+        self.bot_numbers = []
+        self.signal_numbers = []
+
+    def poll_world_for_data(self):
+        for data_list, world_list in ((self.plant_numbers, self.world.plants), (self.bot_numbers, self.world.bots),
+                                      (self.signal_numbers, self.world.signals)):
+            data_list.append(len(world_list))
+
+    def graph_results(self):
+        print("Creating Graph...")
         # Create some graphs to get a sense of what's going on
         graph = plt.figure()
         graph.subplots_adjust(wspace=0.4)
         graph.subplots_adjust(hspace=0.4)
-        plant_dist = graph.add_subplot(2, 2, 1)
-        plant_dist.scatter([plant.x for plant in earth.plants], [plant.y for plant in earth.plants], s=2, lw=0)
-        plant_dist.set_xlabel('X')
-        plant_dist.set_ylabel('Y')
+        entity_dist = graph.add_subplot(2, 2, 1)
+        entity_dist.scatter([plant.x for plant in self.world.plants], [plant.y for plant in self.world.plants],
+                            s=2, lw=0, label='Plants', c='g')
+        entity_dist.scatter([bot.x for bot in self.world.bots], [bot.y for bot in self.world.bots],
+                            s=3, lw=0, label='Bots', c='m')
+        entity_dist.legend(loc='upper left', labelspacing=0, borderpad=0, prop={'size': 7})
+        entity_dist.set_xlabel('X')
+        entity_dist.set_ylabel('Y')
 
         plant_nums = graph.add_subplot(2, 2, 2)
         plant_nums.set_xlabel('Time')
         plant_nums.set_ylabel('Number of Plants')
-        plant_nums.plot(range(0, len(plant_numbers)), plant_numbers)
+        plant_nums.plot(range(0, len(self.plant_numbers)), self.plant_numbers)
 
         bot_dist = graph.add_subplot(2, 2, 3)
-        bot_dist.scatter([bot.x for bot in earth.bots], [bot.y for bot in earth.bots], s=2, lw=0)
+        bot_dist.scatter([bot.x for bot in self.world.bots], [bot.y for bot in self.world.bots], s=2, lw=0)
         bot_dist.set_xlabel('X')
         bot_dist.set_ylabel('Y')
 
         bot_nums = graph.add_subplot(2, 2, 4)
-        bot_nums.plot(range(0, len(bot_numbers)), bot_numbers,)
+        bot_nums.plot(range(0, len(self.bot_numbers)), self.bot_numbers,)
         bot_nums.set_xlabel('Time')
         bot_nums.set_ylabel('Number of Bots')
 
         # Make all subplot axes tick labels smaller and give them a title
-        for subplot, title in [(plant_dist, 'Plant Distribution'), (plant_nums, 'Plant Numbers'),
+        for subplot, title in [(entity_dist, 'Entity Distribution'), (plant_nums, 'Plant Numbers'),
                                (bot_dist, 'Bot Distribution'), (bot_nums, 'Bot Numbers')]:
             subplot.tick_params(labelsize=6)
             subplot.set_title(title)
-
+        print("Saving Graph...")
         graph.savefig(os.getcwd() + os.sep + 'graphs' + os.sep + 'simulation.png', dpi=100)
-
-    graph_results()
 
 
 class GraphicalSimulation:
-    def __init__(self, world):
+    def __init__(self, world, plant_ticks, collect_data=True):
         pygame.init()
         screen_width, screen_height = 500, 500
         if world.boundary_sizes:
@@ -105,11 +168,17 @@ class GraphicalSimulation:
         tick = 0
         self.world = world
         # Run the simulation to get plants spread out
-        earth.add_plant(Plant(screen_width/2, screen_height/2, 5))
-        for i in range(1000):
+        self.world.add_plant(Plant(screen_width/2, screen_height/2, 5))
+        if collect_data:
+            sim_data = SimulationData(self.world)
+        else:
+            sim_data = None
+        for i in range(plant_ticks):
             self.world.step()
+            if sim_data:
+                sim_data.poll_world_for_data()
         # Add a bot and display the simulation
-        behavior = GraphicalSimulation.create_basic_intelligence()
+        behavior = create_basic_intelligence()
         self.world.add_bot(Bot(screen_width/2, screen_height/2, 250, behavior))
         while running:
             for event in pygame.event.get():
@@ -117,11 +186,14 @@ class GraphicalSimulation:
                     running = 0
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     running = 0
-            screen.fill((1, 2, 3))
+            screen.fill((0, 0, 0))
             pygame.display.flip()
             # Do stuff
             # Update the world
             self.world.step()
+            # Collect Data if enabled
+            if sim_data:
+                sim_data.poll_world_for_data()
             # Draw the plants and bots
             pixels = pygame.surfarray.pixels3d(screen)
             for entity_list, color in [(self.world.plants, (40, 200, 40)), (self.world.bots, (150, 40, 150))]:
@@ -140,42 +212,20 @@ class GraphicalSimulation:
             tick += 1
             pygame.display.update()
             self.clock.tick(10)
+        if sim_data:
+            sim_data.graph_results()
         pygame.quit()
 
-    @staticmethod
-    def create_basic_intelligence():
-        check_reproduce_node = ConditionalNode(Bot.can_i_reproduce)
-        create_clone_node = StatementNode(Bot.create_clone)
-        launch_signal_node = StatementNode(Bot.launch_signal)
-        check_active_signal_node = ConditionalNode(Bot.do_i_have_a_signal)
-        check_signal_found_food_node = ConditionalNode(Bot.has_signal_found_food)
-        wait_node = StatementNode(Bot.wait)
-        move_to_target_node = StatementNode(Bot.move_towards_target)
-        check_at_target_node = ConditionalNode(Bot.am_i_near_target)
-        eat_node = StatementNode(Bot.eat_nearby_food)
 
-        check_reproduce_node.assign_edges(create_clone_node, launch_signal_node)
-        create_clone_node.assign_edge(check_reproduce_node)
-        launch_signal_node.assign_edge(check_signal_found_food_node)
-        check_signal_found_food_node.assign_edges(move_to_target_node, wait_node)
-        wait_node.assign_edge(check_active_signal_node)
-        check_active_signal_node.assign_edges(check_signal_found_food_node, launch_signal_node)
-        move_to_target_node.assign_edge(check_at_target_node)
-        check_at_target_node.assign_edges(eat_node, move_to_target_node)
-        eat_node.assign_edge(check_reproduce_node)
-
-        behavior = BehaviorTree()
-        behavior.behavior_nodes = [launch_signal_node, check_reproduce_node, create_clone_node,
-                                   check_signal_found_food_node, wait_node,move_to_target_node, check_at_target_node,
-                                   eat_node, check_active_signal_node]
-        behavior.current_behavior_node = launch_signal_node
-        return behavior
+def run_simulation(world, plant_growth_ticks, additional_ticks, graphics=False, collect_data=True):
+    if graphics:
+        GraphicalSimulation(world, plant_growth_ticks, collect_data=collect_data)
+    else:
+        no_graphics_run(world, plant_growth_ticks, additional_ticks, collect_data=collect_data)
 
 if __name__ == '__main__':
     print("Starting Simulation...")
     start_time = time.time()
-    earth = World(plant_limit=600, boundary_sizes=(500, 500))
-    plant_numbers = []
-    bot_numbers = []
-    GraphicalSimulation(earth)
+    earth = World(plant_limit=200, boundary_sizes=(100, 100))
+    run_simulation(earth, 500, 1000, graphics=True)
     print("Elapsed seconds:", time.time() - start_time)
